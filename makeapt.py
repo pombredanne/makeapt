e1c589d90e99c71ea162065632b9574b5caa5763
Ivan Kosarev <ivan@kosarev.info>
2018-10-06 17:40:42 +0300

    Fix commands usage info.

--- makeapt.py
+++ makeapt.py
@@ -21,18 +21,19 @@ class Repository(object):
 
 class CommandLineDriver(object):
     def __init__(self):
+        self._prog_name = os.path.basename(sys.argv[0])
+
         self.COMMANDS = {
-            'init': self.init,
+            'init': (self.init, 'Initialize APT repository.'),
         }
 
-    def init(self, repo, args):
-        parser = argparse.ArgumentParser(
-            description='Initialize APT repository.')
+    def init(self, repo, parser, args):
         args = parser.parse_args(args)
         repo.init()
 
     def execute_command_line(self, args):
         parser = argparse.ArgumentParser(
+            prog=self._prog_name,
             description='Debian APT repositories generator')
         parser.add_argument('command', help='The command to run.')
 
@@ -43,8 +44,14 @@ class CommandLineDriver(object):
         if args.command not in self.COMMANDS:
             raise Error('Unknown command %r.' % args.command)
 
+        handler, descr = self.COMMANDS[args.command]
+        command_parser = argparse.ArgumentParser(
+            prog='%s %s' % (self._prog_name, args.command),
+            description=descr)
+
         repo = Repository()
-        self.COMMANDS[args.command](repo, command_args)
+
+        handler(repo, command_parser, command_args)
 
     def run(self):
         self.execute_command_line(sys.argv[1:])
